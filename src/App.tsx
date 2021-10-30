dc9dc29002b50f1eb2051c3d66e2a3e7cbf66148
Sami Jan <sami.jan@sybrid.com>
2021-10-30 14:58:53 +0500

    start laying out for mobile and tablet

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,81 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import Typography from "@mui/material/Typography";
+import Breadcrumbs from "@mui/material/Breadcrumbs";
+import Link from "@mui/material/Link";
+import Paper from "@mui/material/Paper";
+import fetchData from "./service";
+import "./App.css";
+import styles from "./App.module.css";
 
 function App() {
+  let [poem, setPoem] = useState(Array);
+
+  useEffect(() => setPoem(fetchData()), [setPoem]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <nav role="presentation">
+          <Breadcrumbs aria-label="breadcrumb" maxItems={2}>
+            <Link underline="hover" color="inherit" href="/">
+              Poetry
+            </Link>
+            <Link
+              underline="hover"
+              color="inherit"
+              href="/getting-started/installation/"
+            >
+              Urdu
+            </Link>
+            <Link
+              underline="hover"
+              color="inherit"
+              href="/getting-started/installation/"
+            >
+              Faraz
+            </Link>
+            <Typography color="text.primary">Suna Hai</Typography>
+          </Breadcrumbs>
+        </nav>
       </header>
+      <main>
+        <Paper elevation={3}>
+          <article className={styles.stanzaBlock}>
+            {poem.map((row, ix) => {
+              if (ix % 2 !== 0) {
+                // even number of row so 2 line stanza finishes and we need a new-line
+                return (
+                  <>
+                    <span
+                      className={styles.stanzaText}
+                      aria-label="stanza"
+                      key={ix}
+                      dir="rtl"
+                    >
+                      <>{row}</>
+                    </span>
+                    <span>
+                      <br></br>
+                      <br></br>
+                    </span>
+                  </>
+                );
+              } else {
+                return (
+                  <span
+                    className={styles.stanzaText}
+                    aria-label="stanza"
+                    key={ix}
+                    dir="rtl"
+                  >
+                    <>{row}</>
+                  </span>
+                );
+              }
+            })}
+          </article>
+        </Paper>
+      </main>
     </div>
   );
 }
